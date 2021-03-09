36f4dfa12a8cd7ad95f639e08ab1423bf292eb89
LadyMisfortunate <80325540+LadyMisfortunate@users.noreply.github.com>
2021-03-09 14:15:27 +0100

    API Controller Section

---
+++ API/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Persistence;
+
+namespace API
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            /*
+             เปลี่ยนจาก CreateHostBuilder(args).Build().Run();
+             เป็น var host = CreateHostBuilder(args).Build();
+             using var scope = host.Services.CreateScope();
+
+             */
+            var host = CreateHostBuilder(args).Build();
+            using var scope = host.Services.CreateScope();
+            var services = scope.ServiceProvider;
+            try
+            {
+                //โค๊ดนี้จะทำให้ Host สร้าง database ถ้ายังไม่มี database
+                var context = services.GetRequiredService<DataContext>();
+                await context.Database.MigrateAsync();
+                await Seed.SeedData(context);
+            }
+            catch(Exception ex)
+            {
+                var logger = services.GetRequiredService<ILogger<Program>>();
+                logger.LogError(ex, "An error occured during migration");
+            }
+
+            //start to run application ถ้าลืมใส่ application จะไม่ทำงาน
+            await host.RunAsync();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
---
+++ Persistence/DataContext.cs
@@ -0,0 +1,34 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        //ถ้าไม่ reference project ของ โมเดล หรือใส่ reference ผิด จะเออเร่อ asp.net No suitable constructor was found for entity type
+        public DbSet<Activity> Activities { get; set; }
+
+
+        /*สร้าง migrations
+         * 1. เลือก Defualt Project ให้ตรงกับโปรเจคที่จะสร้าง migrations
+         * 2. ใส่โค๊ด enable-migrations ที่ package manager console
+         * 3. ใส่โค๊ด "add-migration MigrationName"  ที่ package manager console 
+        */
+
+    }
+}
